2f45fff6d1d12944b8a6d3bc202d9e34f477803c
Haizz <huy20022001@gmail.com>
2025-06-05 16:34:37 +0700

    🚀 Feat: Multi models and refactor code

--- deface_checker.py
+++ deface_checker.py
@@ -4,6 +4,7 @@ import time
 import joblib
 import requests
 import warnings
+from datetime import datetime
 from bs4 import BeautifulSoup, XMLParsedAsHTMLWarning
 import smtplib
 from email.message import EmailMessage
@@ -20,6 +21,8 @@ model_dir="./model"
 vectorizer_name="tfidf_vectorizer.joblib"
 xgb_model_name="model_xgboost.joblib"
 rf_model_name="model_random_forest.joblib"
+ltgbm_model_name="model_lightgbm.joblib"
+xt_model_name="model_extra_trees.joblib"
 
 # Preprocessing function
 def clean_text(text):
@@ -91,15 +94,16 @@ def get_links_from_homepage(base_url, valid_url = ''):  # Placeholder for actual
         valid_url = base_url
 
     try:
-        resp = requests.get(base_url)
+        resp = requests.get(base_url, allow_redirects=False)
         soup = BeautifulSoup(resp.text, 'html.parser')
         links = set()
 
         invalid_ext_pattern = re.compile(r'\.(php|png|jpg|jpeg|gif|bmp|svg|html|asp|aspx|exe)(\?|$)', re.IGNORECASE)
-
         for a in soup.find_all('a', href=True):
+            print(f"Found link >> {a} from {base_url}")
             href = a['href']
-            href = href.replace(valid_url, base_url)
+            # href = href.replace(valid_url, base_url)
+            href = href.replace("http://127.0.0.1", base_url.rstrip('/'))
             if href.startswith(base_url):  # only include internal links
                 if not invalid_ext_pattern.search(href):  # skip links with unwanted extensions
                     links.add(href)
@@ -112,7 +116,7 @@ def get_links_from_homepage(base_url, valid_url = ''):  # Placeholder for actual
 def download_page(url):
     try:
         print(f"Downloading page: {url}")
-        response = requests.get(url, verify=False, timeout=10)
+        response = requests.get(url, verify=False, allow_redirects=False,timeout=10)
         response.raise_for_status()
         return response.text
     except requests.exceptions.RequestException as e:
@@ -146,13 +150,24 @@ def is_valid_html(link):
 def main():
     while True:
         # Get check values from environment variable or use default
-        checking_interval = int(os.getenv('CHECKING_INTERVAL', 600))  
+        checking_interval = int(os.getenv('CHECKING_INTERVAL', 600))
         throttle_time = int(os.getenv('THROTTLE_TIME', 1))
-        base_site = os.getenv('DEFACEMENT_CHECKED_SITE', 'http://localhost:8090/')
+        base_site = os.getenv('DEFACEMENT_CHECKED_SITE', 'http://wordpress:80/')
+        # base_site = 'http://127.0.0.1:80/'  # Default value for testing
+        # base_site = ensure_https(base_site)
+        print(f"\033[33mLink gotten from Env: {repr(base_site)}\033[0m")
+        
+        for _ in range (10):
+            try:
+                requests.get(base_site, allow_redirects=False, timeout=5)
+                break # out of retry loop if successful
+            except requests.ConnectionError:
+                print(f"\033[33mWaiting for {base_site} to be available...\033[0m")
+                time.sleep(5)
         
         links = get_links_from_homepage(base_site)
         for link in links:
-            print(f"{datetime.datetime.now()} >> Checking link: {link}")
+            print(f">> Checking link: {link}")
             if not is_valid_html(link):
                 print(f"\033[33mSkipping {link} (Non-HTML content)\033[0m")
                 continue
@@ -164,10 +179,9 @@ def main():
                 vectorizer = joblib.load(os.path.join(model_dir, vectorizer_name))
                 xgb_model = joblib.load(os.path.join(model_dir, xgb_model_name))
                 rf_model = joblib.load(os.path.join(model_dir, rf_model_name))
-                
+
                 tfidf_vector = vectorizer.transform([preprocessed_text])
-                prediction = xgb_model.predict(tfidf_vector) or rf_model.predict(tfidf_vector)
-                
+                prediction = xgb_model.predict(tfidf_vector) and rf_model.predict(tfidf_vector)
                 if prediction[0] == 1:
                     print(f"\033[31m{link} is defaced.\033[0m")
                     send_alert(link)
